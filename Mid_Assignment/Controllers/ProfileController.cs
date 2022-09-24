2f5ef3f328bff97a7464cbb8eec7956f3935edef
Muntasir Rahman <shakil_muntasir@hotmail.com>
2022-09-24 21:01:57 +0600

    Initial commit

---
+++ Mid_Assignment/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mid_Assignment.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Education()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult References()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mid_Assignment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mid_Assignment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
